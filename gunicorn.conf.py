eb942f070e6a3a7f74e6f3cd2105df1096ec205b
Tomoya Bansho <tomomail1997@gmail.com>
2020-11-12 17:04:19 +0900

    change gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,3 @@
 import os
 
-reload = True
-bind = '127.0.0.1:' + str(os.getenv('PORT', 9876))
-proc_name = 'Infrastructure-Practice-Flask'
-workers = 1
+bind = '0.0.0.0:' + str(os.getenv('PORT', 9876))
